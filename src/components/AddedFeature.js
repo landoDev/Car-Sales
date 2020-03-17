30e54a42af76880f7f3f6581715ec6d985330acc
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-17 16:21:17 -0400

    set up store, provider, and connect

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const AddedFeature = props => {
   return (
     <li>
@@ -10,4 +11,5 @@ const AddedFeature = props => {
   );
 };
 
-export default AddedFeature;
+
+export default AddedFeature ;
---
+++ src/reducers/carReducer.js
@@ -0,0 +1,12 @@
+export const initialState ={
+
+}
+
+export const carReducer = (state, action) =>{
+    switch(action.type){
+        case 'placeholder':
+            return state;
+        default:
+            return state;
+    }
+}
